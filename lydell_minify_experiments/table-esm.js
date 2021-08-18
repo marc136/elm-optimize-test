7b61e90e375e7ee8432b49e5898318f106962338
Marc Walter <walter.marc@outlook.com>
2021-08-18 09:55:55 +0200

    Add lydell_minify_experiments/table-esm.js

---
+++ lydell_minify_experiments/table-esm.js
@@ -0,0 +1,318 @@
+/* eslint-disable no-console */
+const fs = require("fs");
+const path = require("path");
+const zlib = require("zlib");
+const childProcess = require("child_process");
+const UglifyJS = require("uglify-js");
+const esbuild = require("esbuild");
+
+const args = process.argv.slice(2);
+
+if (args.length < 2) {
+  console.error("Must pass at least 2 args: Input.elm output.js");
+  process.exit(1);
+}
+
+const [elmInputFile, elmOutputFile, variantName] = args;
+
+const pureFuncs = [
+  "F2",
+  "F3",
+  "F4",
+  "F5",
+  "F6",
+  "F7",
+  "F8",
+  "F9",
+  "A2",
+  "A3",
+  "A4",
+  "A5",
+  "A6",
+  "A7",
+  "A8",
+  "A9",
+];
+
+function findClosest(name, dir) {
+  const entry = path.join(dir, name);
+  return fs.existsSync(entry)
+    ? entry
+    : dir === path.parse(dir).root
+      ? undefined
+      : findClosest(name, path.dirname(dir));
+}
+
+function elmMakeEsm() {
+  childProcess.spawnSync("elm",
+    ["make", path.resolve(elmInputFile), "--optimize", "--output", path.resolve(elmOutputFile)],
+    { cwd: path.dirname(findClosest("elm.json", path.dirname(elmInputFile))) }
+  );
+
+  const iife = fs.readFileSync(elmOutputFile, 'utf8')
+
+  const esm = iife.slice(
+      iife.indexOf("function F("),
+      iife.lastIndexOf(");}")
+    ).replace('_Platform_export({', 'export const Elm = {')
+
+  fs.writeFileSync(elmOutputFile, esm, 'utf8')
+  return { code: Buffer.from(esm, 'utf8') }
+}
+
+const variants = {
+  "`elm make --optimize` (for reference)": () => elmMakeEsm(),
+  "UglifyJS, only whitespace and comments removal": (code) =>
+    UglifyJS.minify(code, {
+      compress: false,
+      mangle: false,
+    }),
+  "Above, plus shortened variable names (`mangle`)": (code) =>
+    UglifyJS.minify(code, {
+      compress: false,
+      mangle: true,
+    }),
+  "Elm Guide command (run UglifyJS twice: `compress` then `mangle`)": (
+    code
+  ) => {
+    const result = UglifyJS.minify(code, {
+      compress: {
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        unsafe_comps: true,
+        unsafe: true,
+      },
+      mangle: false,
+    });
+    return UglifyJS.minify(result.code, {
+      compress: false,
+      mangle: true,
+    });
+  },
+  "Tweaked Elm Guide command (run UglifyJS just once with `mangle.reserved`)": (
+    code
+  ) =>
+    UglifyJS.minify(code, {
+      compress: {
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        unsafe_comps: true,
+        unsafe: true,
+      },
+      mangle: {
+        reserved: pureFuncs,
+      },
+    }),
+  "Above, plus `passes: 2`": (code) =>
+    UglifyJS.minify(code, {
+      compress: {
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        unsafe_comps: true,
+        unsafe: true,
+        passes: 2,
+      },
+      mangle: {
+        reserved: pureFuncs,
+      },
+    }),
+  "Above, plus `passes: 3`": (code) =>
+    UglifyJS.minify(code, {
+      compress: {
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        unsafe_comps: true,
+        unsafe: true,
+        passes: 3,
+      },
+      mangle: {
+        reserved: pureFuncs,
+      },
+    }),
+  "UglifyJS with Elm Guide `compress` (but no `mangle`) plus esbuild": (
+    code
+  ) => {
+    const result = UglifyJS.minify(code, {
+      compress: {
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        unsafe_comps: true,
+        unsafe: true,
+      },
+      mangle: false,
+    });
+
+    if (result.error !== undefined) {
+      throw result.error;
+    }
+
+    return esbuild.transformSync(result.code, {
+      minify: true,
+      target: "es2020",
+    });
+  },
+  "Parts of UglifyJS plus esbuild": (code) => {
+    const result = UglifyJS.minify(code, {
+      compress: {
+        ...Object.fromEntries(
+          Object.entries(UglifyJS.default_options().compress).map(
+            ([key, value]) => [key, value === true ? false : value]
+          )
+        ),
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        strings: true,
+        sequences: true,
+        merge_vars: true,
+        switches: true,
+        dead_code: true,
+        if_return: true,
+        inline: true,
+        join_vars: true,
+        reduce_vars: true,
+        conditionals: true,
+        collapse_vars: true,
+        unused: true,
+      },
+      mangle: false,
+    });
+
+    if (result.error !== undefined) {
+      throw result.error;
+    }
+
+    return esbuild.transformSync(result.code, {
+      minify: true,
+      target: "es2020",
+      format: "esm",
+    });
+  },
+  "Above, but with the slow option `reduce_vars` disabled": (code) => {
+    const result = UglifyJS.minify(code, {
+      compress: {
+        ...Object.fromEntries(
+          Object.entries(UglifyJS.default_options().compress).map(
+            ([key, value]) => [key, value === true ? false : value]
+          )
+        ),
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        strings: true,
+        sequences: true,
+        merge_vars: true,
+        switches: true,
+        dead_code: true,
+        if_return: true,
+        inline: true,
+        join_vars: true,
+        conditionals: true,
+        collapse_vars: true,
+        unused: true,
+      },
+      mangle: false,
+    });
+
+    if (result.error !== undefined) {
+      throw result.error;
+    }
+
+    return esbuild.transformSync(result.code, {
+      minify: true,
+      target: "es2020",
+    });
+  },
+  "Just esbuild (transform API)": (code) => {
+    return esbuild.transformSync(code, {
+      // bundle: true, // not supported
+      minify: true,
+      pure: pureFuncs,
+      target: "es2020",
+      format: "esm",
+    });
+  },
+  "Just esbuild (build API)": () => {
+    const filename = `elm_${Date.now()}.mjs`;
+    esbuild.buildSync({
+      entryPoints: [elmOutputFile],
+      outfile: filename,
+      // Without 'bundle', unused top level function will not be removed.
+      // but 'bundle' is only allowed for `build` that operates on the file system.
+      // See https://github.com/evanw/esbuild/issues/1551
+      bundle: true,
+      allowOverwrite: true,
+      minify: true,
+      pure: pureFuncs,
+      target: "es2020",
+      format: "esm",
+    });
+    const buffer = fs.readFileSync(filename);
+    fs.rm(filename, err => { if (err) throw err });
+    return { code: buffer };
+  },
+  "UglifyJS tradeoff": (code) =>
+    UglifyJS.minify(code, {
+      compress: {
+        ...Object.fromEntries(
+          Object.entries(UglifyJS.default_options().compress).map(
+            ([key, value]) => [key, value === true ? false : value]
+          )
+        ),
+        pure_funcs: pureFuncs,
+        pure_getters: true,
+        join_vars: true,
+        conditionals: true,
+        unused: true,
+      },
+      mangle: {
+        reserved: pureFuncs,
+      },
+    }),
+  "UglifyJS with all `compress` options off (measure `compress` overhead)": (
+    code
+  ) =>
+    UglifyJS.minify(code, {
+      compress: {
+        ...Object.fromEntries(
+          Object.entries(UglifyJS.default_options().compress).map(
+            ([key, value]) => [key, value === true ? false : value]
+          )
+        ),
+      },
+      mangle: true,
+    }),
+};
+
+if (variantName === undefined) {
+  for (const name of Object.keys(variants)) {
+    childProcess.spawnSync(
+      "node",
+      [__filename, elmInputFile, elmOutputFile, name],
+      { stdio: "inherit" }
+    );
+  }
+  process.exit(0);
+}
+
+const KiB = 1024;
+const MiB = 1024 * KiB;
+
+function printSize(buffer) {
+  return buffer.byteLength >= MiB
+    ? `${(buffer.byteLength / MiB).toFixed(2)}MiB`
+    : `${(buffer.byteLength / KiB).toFixed(0)}KiB`;
+}
+
+const elmCode =
+  variantName === Object.keys(variants)[0]
+    ? ""
+    : fs.readFileSync(elmOutputFile, "utf8");
+const fn = variants[variantName];
+const start = Date.now();
+const { code } = fn(elmCode);
+const end = Date.now();
+const duration = `${((end - start) / 1000).toFixed(1)}s`;
+const buffer = Buffer.from(code);
+const size = printSize(buffer);
+const gzipSize = printSize(zlib.gzipSync(buffer));
+
+console.log(`|${variantName}|${duration}|${size}|${gzipSize}|`);
